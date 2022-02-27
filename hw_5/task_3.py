480ce368f1e620cc137ce6da892029f024466d2f
Denis_Lustgarten <denlust@yandex.ru>
2022-02-27 21:49:02 +0300

    added: list_to_dict, upd: read_file()

--- hw_5/task_3.py
+++ hw_5/task_3.py
@@ -1,21 +1,26 @@
 """3. Создать текстовый файл (не программно). Построчно записать фамилии сотрудников и величину
 их окладов (не менее 10 строк). Определить, кто из сотрудников имеет оклад менее 20 тысяч,
 вывести фамилии этих сотрудников. Выполнить подсчёт средней величины дохода сотрудников."""
-from typing import List, Tuple
+from typing import List, Dict
 from statistics import mean
 
 
 def read_file(filename: str) -> List[str]:
-    employers = []
     with open(filename, 'r', encoding='utf-8') as f:
         employers = f.readlines()
     return employers
 
 
+def list_to_dict(employers: List[str]) -> Dict[str, int]:
+    emp_dict = {person[0]: int(person[1]) for person in [val.strip().split() for val in employers]}
+    return emp_dict
+
+
 if __name__ == '__main__':
-    employers = read_file('test_3.txt')
-    employers = [val.strip().split() for val in employers]
-    print(employers)
-    salary_less_20 = [val[0] for val in employers if int(val[1]) < 20000]
-    print(f'{salary_less_20 = }')
-    average_salary = mean([employers[0] for val in salary_less_20 ])
+    emp_to_list = read_file('test_3.txt')
+    emp_to_dict = list_to_dict(emp_to_list)
+    salary_less_20 = [name for name, sal in emp_to_dict.items() if sal < 20000]
+    print(f'Сотрудники c ЗП меньше 20к: {", ".join(salary_less_20)}')
+
+    average_salary = mean(sal for sal in emp_to_dict.values())
+    print(f'Средняя зарплата = {average_salary:.2f}')

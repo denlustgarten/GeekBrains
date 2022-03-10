73b45a579de5127f58f61c88e0ec5232d1f10af6
Denis_Lustgarten <denlust@yandex.ru>
2022-03-10 23:30:08 +0300

    added: task_1

--- hw_6/task_1.py
+++ hw_6/task_1.py
@@ -6,12 +6,28 @@
 переключение между режимами должно осуществляться только в указанном порядке (красный, жёлтый, зелёный);
 проверить работу примера, создав экземпляр и вызвав описанный метод."""
 
+from colorama import Fore, Style
+from time import sleep
+
 
 class TrafficLight:
-    self.__color = None
 
-    def __init__(self, ):
-        pass
+    def __init__(self):
+        self.__color = ''
 
-    def running(self):
+    def running(self, green_delay: int, cycles_number: int):
+        for i in range(cycles_number):
+            self.__color = 'RED'
+            print(Fore.RED + self.__color)
+            sleep(7)
+            self.__color = 'YELLOW'
+            print(Fore.YELLOW + self.__color)
+            sleep(2)
+            self.__color = 'GREEN'
+            print(Fore.GREEN + self.__color)
+            sleep(green_delay)
 
+
+traffic_lights = TrafficLight()
+traffic_lights.running(green_delay=4, cycles_number=5)
+print(Style.RESET_ALL)
